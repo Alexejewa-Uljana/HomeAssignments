1278dc31c77e827158d9c4c22fdf791636fa1060
Alexejewa-Uljana <st129990@student.spbu.ru>
2024-09-16 12:40:30 +0300

    Add error version

---
+++ hello.h
@@ -0,0 +1,14 @@
+//Uljana Alexejewa st129990@student.spbu.ru Practice-1
+
+#ifndef HELLO_h
+#define HELLO_h
+
+#include <string>
+
+namespace practice-1
+{
+    void SayHello();
+    void SayHello(const std::string &name);
+}
+
+#endif /* HELLO_h */
