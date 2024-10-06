d29ac08d7136c1d634f87048b1cbd67eefe0ddc4
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-06 22:40:22 +0300

    Fix: modified main.cpp file with connection to other files in project

--- Assignment2/Assignment2b/main.cpp
+++ Assignment2/Assignment2b/main.cpp
@@ -1,5 +1,12 @@
-// Алексеева Ульяна
+// Alexejewa Uljana
+
 
 #include <iostream>
+#include <sstream>
+#include <header.h>
+
+int main(int argc, char* argv [])
 {
+    int x = Assignment2b::ReversePolishNotation("2 3 +");
+    std::cout << x;
 }
