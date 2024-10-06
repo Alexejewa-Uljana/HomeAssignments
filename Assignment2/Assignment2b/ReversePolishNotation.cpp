f0233e54431616402ef69b9b40c9fce682908e33
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-06 22:35:50 +0300

    Add: file ReversePolishNotation.cpp

---
+++ Assignment2/Assignment2b/ReversePolishNotation.cpp
@@ -0,0 +1,44 @@
+// Alexejewa Uljana
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <header.h>
+
+int ReversePolishNotation(const std::string input)
+{
+    int *stack = new int[100];
+    int *top_stack = stack;
+    std::stringstream ss(input);
+    std::string sss;
+    while(ss >> sss)
+    {
+        if(sss == "+"){
+            int sum = *top_stack + *(top_stack - 1);
+            top_stack--;
+            stack[top_stack - stack] = sum;
+        }
+        else if(sss == "-"){
+            int sum = *top_stack - *(top_stack - 1);
+            top_stack--;
+            stack[top_stack - stack] = sum;
+        }
+        else if(sss == "*"){
+            int sum = *top_stack * *(top_stack - 1);
+            top_stack--;
+            stack[top_stack - stack] = sum;
+        }
+        else if(sss == "/"){
+            int sum = *top_stack / *(top_stack - 1);
+            top_stack--;
+            stack[top_stack - stack] = sum;
+        }
+        else
+        {
+            top_stack++;
+            stack[top_stack - stack] = std::stoi(sss);
+        }
+
+    }
+    return stack[0];
+}
