a7e3935ae6d5b3e5aedbc3d58c3c86414e50fd07
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-09 17:55:22 +0300

    Fix: add change size of stack and clean memory

--- Assignment2/Assignment2b/RPN.cpp
+++ Assignment2/Assignment2b/RPN.cpp
@@ -1,44 +1,54 @@
-// Alexejewa Uljana
+// Alexejewa Uljana st129990@student.spbu.ru
 
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <header.h>
 
-int rpn(const std::string& input)
+double rpn(const std::string& input)
 {
-    int* stack = new int[100];
-    int* top_stack = stack;
-    std::stringstream ss(input);
-    std::string sss;
-    while(ss >> sss)
+    double* stack = new double[1];
+    int current_size_stack = 1;
+    double* top_stack = stack;
+    std::stringstream s(input);
+    std::string current;
+    while(s >> current)
     {
-        if(sss == "+"){
-            int sum = *top_stack + *(top_stack - 1);
+        if(current == "+"){
+            double sum = *top_stack + *(top_stack - 1);
             top_stack--;
-            stack[top_stack - stack] = sum;
+            *top_stack = sum;
         }
-        else if(sss == "-"){
-            int sum = *top_stack - *(top_stack - 1);
+        else if(current == "-"){
+            double diff = *(top_stack - 1) - *top_stack;
             top_stack--;
-            stack[top_stack - stack] = sum;
+            *top_stack = diff;
         }
-        else if(sss == "*"){
-            int sum = *top_stack * *(top_stack - 1);
+        else if(current == "*"){
+            double multi = *top_stack * *(top_stack - 1);
             top_stack--;
-            stack[top_stack - stack] = sum;
+            *top_stack = multi;
         }
-        else if(sss == "/"){
-            int sum = *top_stack / *(top_stack - 1);
+        else if(current == "/"){
+            double div = *(top_stack - 1) / *top_stack;
             top_stack--;
-            stack[top_stack - stack] = sum;
+            *top_stack = div;
         }
         else
         {
+            double* new_stack = new double[current_size_stack + 1];
+            std::copy(stack, stack + current_size_stack, new_stack);
+            double* t = stack;
+            stack = new_stack;
+            delete [] t;
+            current_size_stack++;
             top_stack++;
-            stack[top_stack - stack] = std::stoi(sss);
+            *top_stack =  std::stod(current);
         }
 
     }
+    double answer = stack[0];
+    delete[] stack;
+    delete top_stack;
     return stack[0];
 }
