33b235eb9a4f78c02f6d2f2aede1547f1240eded
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-18 11:59:38 +0300

    Fix: remove FileToArray.cpp and ArrayToFile.cpp from header.h

--- Assignment2/Assignment2a/header.h
+++ Assignment2/Assignment2a/header.h
@@ -6,9 +6,7 @@
 
 namespace Assignment2a
 {
-    std::pair<char*, uintmax_t> file_to_array(const std::string& path);
     void reverse_array(char* const buffer, const uintmax_t size);
-    void array_to_file(char* const buffer, const uintmax_t size);
 }
 
 
