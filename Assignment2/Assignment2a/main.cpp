8319d46cc2452236bb5e2d627972597c7b549e66
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-18 12:12:30 +0300

    Fix: change uintmax_t to size_t

--- Assignment2/Assignment2a/main.cpp
+++ Assignment2/Assignment2a/main.cpp
@@ -12,18 +12,15 @@ int main(int argc, char* argv [])
     std::cin >> path;
     std::ifstream infile;
     infile.open(path, std::ios::binary|std::ios::in); // open file
-    uintmax_t size_of_file = std::filesystem::file_size(path) - 1; // size of file
+    std::size_t size_of_file = std::filesystem::file_size(path) - 1; // size of file
     char* buffer = new char[size_of_file]; // creat array
     infile.read(buffer, size_of_file); // read file in array
     Assignment2a::reverse_array(buffer, size_of_file);
     std::ofstream outfile; // write result in new file
     outfile.open("Result", std::ios::binary|std::ios::out);
     outfile.write(buffer, size_of_file);
-    
     infile.close(); // close files
     outfile.close();
-    
     delete[] buffer; // clean memory
-    
     return 0;
 }
