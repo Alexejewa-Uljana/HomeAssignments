cb45c7b9855fff1ac2a7c685b17e7ed46c3fe11b
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-18 11:56:19 +0300

    Fix: add in main.cpp read and write files

--- Assignment2/Assignment2a/main.cpp
+++ Assignment2/Assignment2a/main.cpp
@@ -1,14 +1,29 @@
 // Alexejewa_Uljana st129990@student.spbu.ru
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <header.h>
+#include <filesystem>
 
 int main(int argc, char* argv [])
 {
-    std::string  path;
+    std::string  path; // path of file
     std::cin >> path;
-    std::pair <char*, uintmax_t> array_size = Assignment2a::file_to_array(path);
-    Assignment2a::reverse_array(array_size.first, array_size.second);
-    Assignment2a::array_to_file(array_size.first, array_size.second);
+    std::ifstream infile;
+    infile.open(path, std::ios::binary|std::ios::in); // open file
+    uintmax_t size_of_file = std::filesystem::file_size(path) - 1; // size of file
+    char* buffer = new char[size_of_file]; // creat array
+    infile.read(buffer, size_of_file); // read file in array
+    Assignment2a::reverse_array(buffer, size_of_file);
+    std::ofstream outfile; // write result in new file
+    outfile.open("Result", std::ios::binary|std::ios::out);
+    outfile.write(buffer, size_of_file);
+    
+    infile.close(); // close files
+    outfile.close();
+    
+    delete[] buffer; // clean memory
+    
     return 0;
 }
