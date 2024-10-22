9eeb53cf921772fae2f8cf798b5672f39ed4857f
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-22 21:25:45 +0300

    Add: realization functions for Degree class

---
+++ Assignment3/Degree.cpp
@@ -0,0 +1,26 @@
+// Алексеева Ульяна st129990@student.spbu.ru
+
+#include <iostream>
+#include "Degree.h"
+
+Degree::Degree() {
+    _degree = 0;
+    _period = 0;
+}
+
+void Degree::setDegree(uint degree) {
+   _degree = degree % 360;
+   _period += degree / 360;
+}
+
+uint Degree::getDegree() {
+    return _degree;
+}
+
+void Degree::setPeriod(uint period) {
+    _period += period;
+}
+
+uint Degree::getPeriod() {
+    return _period;
+}
