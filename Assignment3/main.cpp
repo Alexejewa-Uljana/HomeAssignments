636c0e397b1caf0f7bffd202f1a10dd547ec1daf
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-22 21:17:16 +0300

    Add: include Autobot.h Decepticon.h Shifter.h Degree.h

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include "Transformer.h"
 #include "Gun.h"
+#include "Degree.h"
+#include "Autobot.h"
+#include "Decepticon.h"
+#include "Shifter.h"
+
 int main()
 {
     return 0;
