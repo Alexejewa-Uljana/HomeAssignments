ec724b8c177f011d139aa50a9a61644370da9095
Alexejewa-Uljana <alexejewa.uljana@gmail.com>
2024-10-22 21:22:12 +0300

    Fix: fix syntax errors

--- Assignment3/Decepticon.h
+++ Assignment3/Decepticon.h
@@ -1,8 +1,10 @@
 // Алексеева Ульяна st129990@student.spbu.ru
 
 #include <iostream>
-#include "Transformer.h"
 #include "Gun.h"
+#include "Degree.h"
+#include "Transformer.h"
+
 
 class Decepticon: public Transformer
 {
@@ -11,7 +13,7 @@ public:
     void setLevel_of_evil(uint level_of_evil);
     uint getLevel_of_evil();
     void setLevel_of_deception(uint level_of_deception);
-    uint detLevel_of_deception();
+    uint getLevel_of_deception();
     bool transform();
     bool evil_attack();
     bool act_of_kidness();
